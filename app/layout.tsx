180d527863150e06933a71874765ddd13dde3e32
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-24 00:47:20 +0530

    Migrate to NextJS 13.5

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Portfolio | Anurag Yadav',
+  description: 'नमस्ते! 🙏🏻 Anurag Yadav is a software engineer based out of Mumbai, India.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <p>
+        Hi, I am Anurag Yadav, a software engineer, full stack developer, and a
+        tech enthusiast. I am currently working at Mithya Labs as a junior full
+        stack developer.
+      </p>
+    </main>
+  );
+}
