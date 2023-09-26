e05c63e54af547690533489e7435d635b6a442c0
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-27 01:58:43 +0530

    Update Layout @ Root

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
-import Typography from "./components/Typography";
+import Navbar from "./components/Navbar";
+import HeroSection from "./sections/hero";
 
 export default function Home() {
     return (
-        <main className="px-5 flex flex-col min-h-screen items-center justify-center">
-            <Typography variant="h1">Coming Soon...</Typography>
-            <Typography variant="h2">In a portfolio near you</Typography>
-            <Typography variant="body" color="primary">
-                Hi, I am Anurag Yadav, a software engineer, full stack
-                developer, and a tech enthusiast. I am currently working at
-                Mithya Labs as a junior full stack developer.
-            </Typography>
-        </main>
+        <>
+            <Navbar />
+            <main>
+                <HeroSection />
+            </main>
+        </>
     );
 }
