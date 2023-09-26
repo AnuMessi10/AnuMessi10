f1ce914a26d5ae7720364de0fea7e11af1fcb338
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-27 01:57:55 +0530

    Create Navbar

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Link from "next/link";
+import React, { FC } from "react";
+import styles from "./index.module.scss";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+import useIsMobile from "@/app/hooks/useIsMobile";
+
+export interface INavbarProps {}
+
+const Navbar: FC<INavbarProps> = (props) => {
+    const pathname = usePathname();
+    const { isSmallDevice } = useIsMobile();
+
+    return (
+        <nav className={twMerge(styles.navbar, "blade")}>
+            <div className={styles["navlinks-ctn"]}>
+                {isSmallDevice ? (
+                    <div>Icon</div>
+                ) : (
+                    <>
+                        {navLinks.map(({ name, href }) => (
+                            <Link
+                                href={href}
+                                key={name}
+                                className={twMerge(
+                                    styles.navlink,
+                                    pathname === href &&
+                                        "!font-bold !text-brand-900"
+                                )}
+                            >
+                                {name}
+                            </Link>
+                        ))}
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+const navLinks = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "Projects",
+        href: "/#projects",
+    },
+    {
+        name: "About Me",
+        href: "/#about",
+    },
+    {
+        name: "Contact",
+        href: "/#contact",
+    },
+];
+
+export default Navbar;
