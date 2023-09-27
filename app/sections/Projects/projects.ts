744ef2ac0562281d191577bda07df30c172386fc
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-28 02:18:45 +0530

    Add Background + Z Index to NavBar

--- app/sections/Projects/projects.ts
+++ app/sections/Projects/projects.ts
@@ -1,3 +1,40 @@
 import { IProjectCardProps } from "@/app/components/ProjectCard";
 
-export const projectCardsConfig: IProjectCardProps[] = [];
+export const projectCardsConfig: IProjectCardProps[] = [
+    {
+        image: "https://placehold.jp/400x300.png",
+        title: "Food Supply Chain Using Hyperledger Fabric",
+        description:
+            "A blockchain based food supply chain system using Hyperledger Fabric.",
+        tags: ["Blockchain", "Hyperledger Fabric"],
+        demoLink: "https://www.google.com",
+        repoLink: "https://github.com/AnuMessi10/",
+    },
+    {
+        image: "https://placehold.jp/400x300.png",
+        title: "Food Supply Chain Using Hyperledger Fabric",
+        description:
+            "A blockchain based food supply chain system using Hyperledger Fabric.",
+        tags: ["Blockchain", "Hyperledger Fabric", "Supply Chain"],
+        demoLink: "https://www.google.com",
+        repoLink: "https://github.com/AnuMessi10/",
+    },
+    {
+        image: "https://placehold.jp/400x300.png",
+        title: "Food Supply Chain Using Hyperledger Fabric",
+        description:
+            "A blockchain based food supply chain system using Hyperledger Fabric.",
+        tags: ["Blockchain", "Hyperledger Fabric", "Supply Chain"],
+        demoLink: "https://www.google.com",
+        repoLink: "https://github.com/AnuMessi10/",
+    },
+    {
+        image: "https://placehold.jp/400x300.png",
+        title: "Food Supply Chain Using Hyperledger Fabric",
+        description:
+            "A blockchain based food supply chain system using Hyperledger Fabric.",
+        tags: ["Blockchain", "Hyperledger Fabric", "Supply Chain"],
+        demoLink: "https://www.google.com",
+        repoLink: "https://github.com/AnuMessi10/",
+    },
+];
