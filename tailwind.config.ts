f7d62830a565c71e9647b95bef230b2d28bb7617
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-28 02:14:14 +0530

    Add Silver to Palette ➕ Override Border Radius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: { 100: "#F4DEBE", 500: "#BF7506", 900: "#BF7506" },
-        dark: { 100: "#E7E6E4", 500: "#83807C", 900: "#211B12" },
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: { 100: "#F4DEBE", 500: "#BF7506", 900: "#BF7506" },
+                dark: { 100: "#E7E6E4", 500: "#83807C", 900: "#211B12" },
+                silver: { 100: "#ecebff" },
+            },
+            borderRadius: {
+                xs: "2px",
+                sm: "4px",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
