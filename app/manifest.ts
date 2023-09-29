52d72121d40091eacd194d64f217887f8cbf6357
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-09-29 23:12:12 +0530

    Fix: File Paths

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,32 +12,32 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: "#DD9C3C",
         icons: [
             {
-                src: "/app/favicon.ico",
-                sizes: "any",
+                src: "/favicon.ico",
+                sizes: "48x48",
                 type: "image/x-icon",
             },
             {
-                src: "/app/apple-touch-icon.png",
+                src: "/apple-icon.png",
                 sizes: "180x180",
                 type: "image/png",
             },
             {
-                src: "/app/public/android-chrome-192x192.png",
+                src: "/android-chrome-192x192.png",
                 sizes: "192x192",
                 type: "image/png",
             },
             {
-                src: "/app/public/android-chrome-512x512.png",
+                src: "/android-chrome-512x512.png",
                 sizes: "512x512",
                 type: "image/png",
             },
             {
-                src: "/app/public/favicon-16x16.png",
+                src: "/favicon-16x16.png",
                 sizes: "16x16",
                 type: "image/png",
             },
             {
-                src: "/app/public/favicon-32x32.png",
+                src: "/favicon-32x32.png",
                 sizes: "32x32",
                 type: "image/png",
             },
